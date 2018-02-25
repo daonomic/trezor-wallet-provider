b38ff6afd64ec40f2326579eea5714a3fec9c876
0v1se <dev@0v1se.com>
2018-02-25 13:34:33 +0300

    initial

--- index.js
+++ index.js
@@ -1,69 +1,163 @@
-var bip39 = require("bip39");
-var hdkey = require('ethereumjs-wallet/hdkey');
+'use strict';
+
 var ProviderEngine = require("web3-provider-engine");
 var FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js');
 var HookedSubprovider = require('web3-provider-engine/subproviders/hooked-wallet.js');
 var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
 var Web3 = require("web3");
 var Transaction = require('ethereumjs-tx');
+var trezor = require('trezor.js-node');
+var util = require('util');
 
-function HDWalletProvider(mnemonic, provider_url, address_index=0, num_addresses=1) {
-  this.mnemonic = mnemonic;
-  this.hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
-  this.wallet_hdpath = "m/44'/60'/0'/0/";
-  this.wallets = {};
-  this.addresses = [];
+var debug = false;
 
-  for (let i = address_index; i < address_index + num_addresses; i++){
-    var wallet = this.hdwallet.derivePath(this.wallet_hdpath + i).getWallet();
-    var addr = '0x' + wallet.getAddress().toString('hex');
-    this.addresses.push(addr);
-    this.wallets[addr] = wallet;
-  }
+function normalize(hex) {
+	if (hex == null) {
+		return null;
+	}
+	if (hex.startsWith("0x")) {
+		hex = hex.substring(2);
+	}
+	if (hex.length % 2 != 0) {
+		hex = "0" + hex;
+	}
+	return hex;
+}
 
-  const tmp_accounts = this.addresses;
-  const tmp_wallets = this.wallets;
+function buffer(hex) {
+	if (hex == null) {
+		return new Buffer('', 'hex');
+	} else {
+		return new Buffer(normalize(hex), 'hex');
+	}
+}
 
-  this.engine = new ProviderEngine();
-  this.engine.addProvider(new HookedSubprovider({
-    getAccounts: function(cb) { cb(null, tmp_accounts) },
-    getPrivateKey: function(address, cb) {
-      if (!tmp_wallets[address]) { return cb('Account not found'); }
-      else { cb(null, tmp_wallets[address].getPrivateKey().toString('hex')); }
-    },
-    signTransaction: function(txParams, cb) {
-      let pkey;
-      if (tmp_wallets[txParams.from]) { pkey = tmp_wallets[txParams.from].getPrivateKey(); }
-      else { cb('Account not found'); }
-      var tx = new Transaction(txParams);
-      tx.sign(pkey);
-      var rawTx = '0x' + tx.serialize().toString('hex');
-      cb(null, rawTx);
+
+/**
+ * @param {string}
+ */
+function buttonCallback(label, code) {
+    if (debug) {
+        // We can (but don't necessarily have to) show something to the user, such
+        // as 'look at your device'.
+        // Codes are in the format ButtonRequest_[type] where [type] is one of the
+        // types, defined here:
+        // https://github.com/trezor/trezor-common/blob/master/protob/types.proto#L78-L89
+        console.log('User is now asked for an action on device', code);
     }
-  }));
-  this.engine.addProvider(new FiltersSubprovider());
-  this.engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(provider_url)));
-  this.engine.start(); // Required by the provider engine.
+    console.log("Look at device " + label + " and press the button, human.");
+}
+
+/**
+ * @param {Function<Error, string>} callback
+ */
+function passphraseCallback(callback) {
+    console.log('Please enter passphrase.');
+
+    // note - disconnecting the device should trigger process.stdin.pause too, but that
+    // would complicate the code
+
+    // we would need to pass device in the function and call device.on('disconnect', ...
+
+    process.stdin.resume();
+    process.stdin.on('data', function (buffer) {
+        var text = buffer.toString().replace(/\n$/, "");
+        process.stdin.pause();
+        callback(null, text);
+    });
+}
+
+var list;
+var dev;
+
+// you should do this to release devices on exit
+function TrezorProvider(provider_url, path) {
+	list = new trezor.DeviceList({debug: debug});
+	list.on('connect', function (device) {
+		dev = device;
+        if (debug) {
+            console.log('Connected a device:', device);
+            console.log('Devices:', list.asArray());
+        }
+        console.log("Connected device " + device.features.label);
+
+	    device.on('pin', (type, callback) => {
+	        console.error("Entering pin is not supported. Unlock TREZOR in other app!");
+	        callback(new Error());
+	    });
+
+        // For convenience, device emits 'disconnect' event on disconnection.
+        device.on('disconnect', function () {
+            console.log("Disconnected device");
+            dev = null;
+        });
+
+        // You generally want to filter out devices connected in bootloader mode:
+        if (device.isBootloader()) {
+            throw new Error('Device is in bootloader mode, re-connected it');
+        }
+
+    });
+
+    list.on('error', function (error) {
+        //console.error('List error:', error);
+    });
+
+    process.on('exit', function() {
+        list.onbeforeunload();
+    });
+
+	function inTrezorSession(cb) {
+		if (dev == null) {
+			return Promise.reject(new Error("no device connected"));
+		} else {
+		    return dev.waitForSessionAndRun(cb);
+		}
+	}
+
+	this.engine = new ProviderEngine();
+	this.engine.addProvider(new HookedSubprovider({
+	getAccounts: function(cb) {
+	    inTrezorSession(
+	        session => session.ethereumGetAddress(path, false)
+	    )
+	    .then(resp => "0x" + resp.message.address)
+	    .then(address => {cb(null, [address]); console.log("address: " + address)})
+	    .catch(cb);
+	},
+	signTransaction: function(txParams, cb) {
+		inTrezorSession(
+			session => session.signEthTx(path, normalize(txParams.nonce), normalize(txParams.gasPrice), normalize(txParams.gas), normalize(txParams.to), normalize(txParams.value), normalize(txParams.data))
+		)
+		.then(result => {
+			var tx = new Transaction({
+			   nonce: buffer(txParams.nonce),
+			   gasPrice: buffer(txParams.gasPrice),
+			   gasLimit: buffer(txParams.gas),
+			   to: buffer(txParams.to),
+			   value: buffer(txParams.value),
+			   data: buffer(txParams.data),
+			   v: result.v,
+			   r: buffer(result.r),
+			   s: buffer(result.s)
+			});
+			cb(null, '0x' + tx.serialize().toString('hex'));
+		})
+		.catch(cb);
+	}
+	}));
+	this.engine.addProvider(new FiltersSubprovider());
+	this.engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(provider_url)));
+	this.engine.start(); // Required by the provider engine.
 };
 
-HDWalletProvider.prototype.sendAsync = function() {
+TrezorProvider.prototype.sendAsync = function() {
   this.engine.sendAsync.apply(this.engine, arguments);
 };
 
-HDWalletProvider.prototype.send = function() {
+TrezorProvider.prototype.send = function() {
   return this.engine.send.apply(this.engine, arguments);
 };
 
-// returns the address of the given address_index, first checking the cache
-HDWalletProvider.prototype.getAddress = function(idx) {
-  console.log('getting addresses', this.addresses[0], idx)
-  if (!idx) { return this.addresses[0]; }
-  else { return this.addresses[idx]; }
-}
+module.exports = TrezorProvider;
 
-// returns the addresses cache
-HDWalletProvider.prototype.getAddresses = function() {
-  return this.addresses;
-}
-
-module.exports = HDWalletProvider;
