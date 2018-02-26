f34faa43af04e8e00d31f2202ea6bd34feb8a95f
0v1se <dev@0v1se.com>
2018-02-26 11:49:58 +0300

    implemented as subprovider

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 'use strict';
 
-var ProviderEngine = require("web3-provider-engine");
-var FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js');
 var HookedSubprovider = require('web3-provider-engine/subproviders/hooked-wallet.js');
-var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
-var Web3 = require("web3");
 var Transaction = require('ethereumjs-tx');
 var trezor = require('trezor.js-node');
 var util = require('util');
@@ -32,17 +28,13 @@ function buffer(hex) {
 	}
 }
 
-class TrezorProvider {
-	constructor(url, path) {
+class Trezor {
+	constructor(path) {
 		var self = this;
-		this.url = url;
+
 		this.path = path;
 		this.list = new trezor.DeviceList({debug: debug});
 		this.list.on('connect', function (device) {
-	        if (debug) {
-	            console.log('Connected a device:', device);
-	            console.log('Devices:', this.list.asArray());
-	        }
 	        console.log("Connected device " + device.features.label);
 
 		    device.on('pin', (type, callback) => {
@@ -59,47 +51,12 @@ class TrezorProvider {
 	        if (device.isBootloader()) {
 	            throw new Error('Device is in bootloader mode, re-connected it');
 	        }
-
 	    });
-
-		this.engine = new ProviderEngine();
-		this.engine.addProvider(new HookedSubprovider({
-			getAccounts: function(cb) {
-			    self.inTrezorSession(
-			        session => session.ethereumGetAddress(path, false)
-			    )
-			    .then(resp => "0x" + resp.message.address)
-			    .then(address => {cb(null, [address]); console.log("address: " + address)})
-			    .catch(cb);
-			},
-			signTransaction: function(txParams, cb) {
-				self.inTrezorSession(
-					session => session.signEthTx(path, normalize(txParams.nonce), normalize(txParams.gasPrice), normalize(txParams.gas), normalize(txParams.to), normalize(txParams.value), normalize(txParams.data))
-				)
-				.then(result => {
-					var tx = new Transaction({
-					   nonce: buffer(txParams.nonce),
-					   gasPrice: buffer(txParams.gasPrice),
-					   gasLimit: buffer(txParams.gas),
-					   to: buffer(txParams.to),
-					   value: buffer(txParams.value),
-					   data: buffer(txParams.data),
-					   v: result.v,
-					   r: buffer(result.r),
-					   s: buffer(result.s)
-					});
-					cb(null, '0x' + tx.serialize().toString('hex'));
-				})
-				.catch(cb);
-			}
-		}));
-		this.engine.addProvider(new FiltersSubprovider());
-		this.engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(url)));
-		this.engine.start(); // Required by the provider engine.
 	}
 
 	inTrezorSession(cb) {
-		var devices = this.list.asArray();
+        const devices = this.list.asArray();
+        console.log(devices);
         if (devices.length == 0) {
             return Promise.reject(new Error("no device connected"));
         } else {
@@ -107,15 +64,53 @@ class TrezorProvider {
         }
     }
 
+	getAccounts(cb) {
+		var self = this;
+	    this.inTrezorSession(
+	        session => session.ethereumGetAddress(self.path, false)
+	    )
+	    .then(resp => "0x" + resp.message.address)
+	    .then(address => {cb(null, [address]); console.log("address: " + address)})
+	    .catch(cb);
+	}
+
+	signTransaction(txParams, cb) {
+		var self = this;
+		this.inTrezorSession(
+			session => session.signEthTx(self.path, normalize(txParams.nonce), normalize(txParams.gasPrice), normalize(txParams.gas), normalize(txParams.to), normalize(txParams.value), normalize(txParams.data))
+		)
+		.then(result => {
+			const tx = new Transaction({
+			   nonce: buffer(txParams.nonce),
+			   gasPrice: buffer(txParams.gasPrice),
+			   gasLimit: buffer(txParams.gas),
+			   to: buffer(txParams.to),
+			   value: buffer(txParams.value),
+			   data: buffer(txParams.data),
+			   v: result.v,
+			   r: buffer(result.r),
+			   s: buffer(result.s)
+			});
+			cb(null, '0x' + tx.serialize().toString('hex'));
+		})
+		.catch(cb);
+	}
+
 }
 
-TrezorProvider.prototype.sendAsync = function() {
-  this.engine.sendAsync.apply(this.engine, arguments);
-};
-
-TrezorProvider.prototype.send = function() {
-  return this.engine.send.apply(this.engine, arguments);
-};
+class TrezorProvider extends HookedSubprovider {
+	constructor(path) {
+		var trezor = new Trezor(path);
+		super({
+			getAccounts: function(cb) {
+				trezor.getAccounts(cb);
+			},
+			signTransaction: function(txParams, cb) {
+				trezor.signTransaction(txParams, cb);
+			}
+		});
+	}
+}
 
 module.exports = TrezorProvider;
 
